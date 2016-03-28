5a2f69a32b47f34691f112e29d26328f7c094c2b
TheKenCode <artist13kennyg13@gmail.com>
2016-03-28 11:27:46 -0700

    fixing garbage

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$('#submit').click(function(){
-		searchHistory();
-		//window.location.replace("https://api.instagram.com/oauth/authorize/?client_id=27292a5037854cd2b819fb12fb114642&redirect_uri=https://thekencode.github.io&response_type=code");
+		
+		window.location.replace("https://api.instagram.com/oauth/authorize/?client_id=27292a5037854cd2b819fb12fb114642&redirect_uri=https://thekencode.github.io&response_type=code");
+		
 		var searchKey = {
 			q: $("#search").val()
 		};
@@ -12,29 +13,34 @@ $(document).ready(function(){
 		//Should be getting the access token
 
 		var access_token = document.URL;
+		
 		for(var i = 0; i < access_token.length; i++){
 			if(access_token.charAt(i) == '=' && access_token.charAt(i + 1) >= '0'){
 				access_token = access_token.substring(i + 1, access_token.length);
 				break;
 			}
 		};
+		
 		console.log(access_token);
 		
+		gettingSearch("bowery", access_token);
+		
+	});
+	function gettingSearch(var search, var access_token){
 		$.ajax({
-			url: "https://api.instagram.com/v1/tags/search?q="+ $("#search").val() + "&access_token=" + access_token,
+			url: "https://api.instagram.com/v1/tags/search?q="+ search + "&access_token=" + access_token,
 			dataType: 'jsonp',
 			success: function(data){
 				console.log(data);
 			}
-
-		});
-	});
+		});	
+	}
 	function searchHistory(){
 		console.log("History: ");
 		$.ajax({
 			type: 'POST',
-			url: 'TheKenCode.github.io/searchHistroy.txt.txt',
-			data: $("#search").val(),
+			url: 'searchHistroy.txt.txt',
+			data: {"Search": $("#search").val()},
 			success: function(data){
 				console.log(data);
 			}
