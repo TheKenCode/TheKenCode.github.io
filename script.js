9394132a188d39ec1a0572c6cfaa63974b9d67d0
TheKenCode <artist13kennyg13@gmail.com>
2016-03-27 17:46:13 -0700

    Adding functions

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 	$('#submit').click(function(){
-		console.log($("#search").value());
+		var searchKey = $("#search").val();
 		
+		$.ajax({
+			url: "http://search.twitter.com/search.json?" + $.param(searchKey),
+			dataType: 'jsonp',
+			success: function(data){
+				for(item in data['results']){
+					$('#twitter').append(
+					'<li>' + data['results'][item]['profile_image_url'] + "<li>"
+					);
+				}
+			}
+
+		});
 	});
 	
 	
