28362358b55edcbaa968c61f42072ca78fad5086
TheKenCode <artist13kennyg13@gmail.com>
2016-03-27 19:46:17 -0700

    substring

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		var access_token = document.URL;
 		for(var i = 0; i < access_token.length; i++){
 			if(access_token.char(i) == '=' && access_token.char(i + 1) >= '0'){
-				access_token = access_token(i + 1, access_token.length);
+				access_token = access_token.substring(i + 1, access_token.length);
 				break;
 			}
 		};
