4c69d88f17584d6f141831515399a0e3826c6929
TheKenCode <artist13kennyg13@gmail.com>
2016-03-27 18:08:16 -0700

    Search

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
 	$('#submit').click(function(){
-		var searchKey = $("#search").val();
+		var searchKey = {
+			q: $("#search").val();
+		};
+
 		console.log("Searching for " + searchKey);
 		$.ajax({
 			url: "http://search.twitter.com/search.json?" + $.param(searchKey),
 			dataType: 'jsonp',
 			success: function(data){
+				console.log("success");
 				for(item in data['results']){
 					$('#twitter').append(
-					'<li>' + data['results'][item]['profile_image_url'] + "<li>"
+					'<li>' + data['results'][item]['profile_image_url'] + "</li>"
 					);
-				}
+				};
 			}
 
 		});
 	});
-	
 	
 	
 });
