1947bf1694d46ff0da1fdf37e61686382cd86d1f
TheKenCode <artist13kennyg13@gmail.com>
2016-03-27 19:26:03 -0700

    Garbage

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 		var searchKey = {
 			q: $("#search").val()
 		};
-
-		//console.log("Searching for " + searchKey);
+		//27292a5037854cd2b819fb12fb114642
+		//9166072075b447239abdafc592fb52ab
+		//https://thekencode.github.io
 		$.ajax({
 			url: "https://api.instagram.com/v1/tags/search?q="+ $("#search").val() + "&client_id=27292a5037854cd2b819fb12fb114642", //$.param(searchKey),
 			dataType: 'jsonp',
