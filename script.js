7928af3bcdb951a2c0ec4c6232ce0df628bbeff8
TheKenCode <artist13kennyg13@gmail.com>
2016-03-28 10:23:04 -0700

    Text File Get

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 	});
 	function searchHistory(){
 		$.ajax({
-			url: 'https://thekencode.github.io/searchHistroy.txt',
+			url: '/searchHistroy.txt',
 			success: function(data){
 				console.log(data);
 			}
