3210ccb42a9ab8b42b7cd1fd3e90c7883c1c5c13
TheKenCode <artist13kennyg13@gmail.com>
2016-03-28 18:50:27 -0700

    Quick Load

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
 	var haveKey = false;
+	gettingCodeAndToken();
 	$('#submit').click(function(){
 		haveKey =  false;
 		
+		var searchKey = $("#search").val();
+
+		var access_token = document.URL;
+		
+		for(var i = 0; i < access_token.length; i++){
+			if(access_token.charAt(i) == '=' && access_token.charAt(i + 1) >= '0'){
+				access_token = access_token.substring(i + 1, access_token.length);
+				break;
+			}
+			else if(i == access_token.length - 1){
+				access_token = "";
+				break;
+			}
+		};
+		
+		console.log(access_token);
+		gettingSearch(searchKey, access_token);
+	});
+	function gettingCodeAndToken(){
 		if(document.URL != "https://thekencode.github.io/" && document.URL != "http://thekencode.github.io/"){
 			haveKey = true;
 			console.log("changed");
@@ -23,25 +43,7 @@ $(document).ready(function(){
 			console.log("Getting Code");
 			gettingToken("code");
 		}
-		
-		var searchKey = $("#search").val();
-
-		var access_token = document.URL;
-		
-		for(var i = 0; i < access_token.length; i++){
-			if(access_token.charAt(i) == '=' && access_token.charAt(i + 1) >= '0'){
-				access_token = access_token.substring(i + 1, access_token.length);
-				break;
-			}
-			else if(i == access_token.length - 1){
-				access_token = "";
-				break;
-			}
-		};
-		
-		console.log(access_token);
-		gettingSearch(searchKey, access_token);
-	});
+	};
 	function gettingToken(type){
 		window.location.replace("https://api.instagram.com/oauth/authorize/?client_id=27292a5037854cd2b819fb12fb114642&redirect_uri=https://thekencode.github.io&scope=public_content&response_type=" + type);
 	}
